41f33411dc8a36032af7e3d81e9e9bc0aede6eed
1 <jihoon-lim>
2022-12-15 12:01:56 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Controls;
+
+namespace winrt::App1::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
+
+
+void winrt::App1::implementation::MainWindow::toggleOpen_Toggled(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    if (toggleOpen().IsOn()) {
+        Hello().ZoomMode(ZoomMode::Disabled);
+        Hello().ZoomToFactor(1);
+        ZoomSlider().Value(1);
+        ZoomSlider().IsEnabled(FALSE);
+    }
+    else{
+        Hello().ZoomMode(ZoomMode::Enabled);
+        ZoomSlider().IsEnabled(TRUE);
+    }
+}
+
+void winrt::App1::implementation::MainWindow::ZoomSlider_ValueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e)
+{
+    mySize = e.NewValue();
+    Hello().ZoomToFactor(mySize);
+}
---
+++ MainWindow.xaml.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::App1::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        float mySize;
+        
+        void ScrollViewer_ManipulationCompleted(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::ManipulationCompletedRoutedEventArgs const& e);
+        void toggleOpen_Toggled(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void ZoomSlider_ValueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e);
+    };
+}
+
+namespace winrt::App1::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
